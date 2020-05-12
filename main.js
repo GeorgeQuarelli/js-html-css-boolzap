d6407bfa50d945895f89ea70b3c68408eea0b839
George <quarelli.george@gmail.com>
2020-05-12 19:23:13 +0200

    milestone 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //intercettiamo l'input dove verra scritto il messaggio
 $('#insert-messaggi').keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
+    var keycode = (event.which);
     if(keycode == '13'){
         // leggo il valore destritto inserito nel input
         var messaggio_utente = $('#insert-messaggi').val();
@@ -10,7 +10,37 @@ $('#insert-messaggi').keypress(function(event){
             $('.container_chat').append(ilMioNuovoMessaggio);
             // input reset
             $('#insert-messaggi').val('');
+            // rimetto il messaggio di default
             $(this).attr('placeholder', 'Scrivi un messaggio');
+            //inseriamo la risposta del computer automatica
+            setTimeout(function(){
+            var pcmessaggio = $('.container_chat').append('<li class="messaggio-ricevuto">' + 'ok' + '</li>'); }, 100);
         }
     }
+});
+
+//scrivendo qualcosa nell’input a sinistra
+$('.input-container').keyup(function(event){
+        var testo_utente = $('.input-container').val().trim().toLowerCase();
+        console.log('testo utente:' + testo_utente);
+        if (testo_utente != '') {
+            $('.chat_mex').each(function() {
+            // facciamo in modo che il testo sia considerato tutto minuscolo
+            var testo_h4 = $(this).find('h4').text().trim().toLowerCase();
+            // // stampo il testo di ogni h4
+            // console.log('testo h4:' + testo_h4);
+            // verifico se il testo inserito è uguale a quello inserito
+            if (testo_h4.includes(testo_utente)) {
+                // se è uguale mostro la chat dell'utente corretto
+                $(this).show();
+            }
+            else {
+                // se non corrisponde il contatto con il nome inserito viene nascosto
+                $(this).hide();
+            }
+        })
+        }
+        else {
+            $('.chat_mex').show();
+        }
 });
